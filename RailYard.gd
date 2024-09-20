424ed2aa296ee51b3a7d030d0ea3b74dcf794b04
Andrew Nathenson <colorfulpockets01@gmail.com>
2024-09-19 17:18:55 -0700

    Added Research and Resupply as options to Rail Yard

--- RailYard.gd
+++ RailYard.gd
@@ -1,14 +1,24 @@
 extends Control
 
+signal choice_made
+
 var RefactorScene = preload("res://refactor_screen.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$"MarginContainer/HBoxContainer/Research/Research Button".pressed.connect(research)
+	$"MarginContainer/HBoxContainer/Resupply/Resupply Button".pressed.connect(resupply)
+	
+
+func resupply():
+	Stats.addEmergencyRail(5)
+	choice_made.emit()
+
+func research():
 	var selectCardsView = DeckView.new(Stats.deck, 3)
 	add_child(selectCardsView)
 	selectCardsView.backButtonPressed.connect(func(): 
 		selectCardsView.queue_free()
-		# TODO: Make it a skip button instead of Back, or add another option or something
 		)
 	selectCardsView.confirmPressed.connect(func(cardsSelected):
 		for cardName in cardsSelected:
@@ -18,12 +28,12 @@ func _ready():
 			func(refactoredCards):
 				Stats.deck += refactoredCards
 				refactorScene.queue_free()
+				choice_made.emit()
 		)
 		add_child(refactorScene)
 		refactorScene.populateCards(cardsSelected)
 		selectCardsView.queue_free()
 		)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
